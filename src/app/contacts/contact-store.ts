89b85ec6da61f612b631bc13282308f2919a07ea
Mario Daza <mariodaza525@gmail.com>
2026-05-06 01:01:35 +0200

    feat: implement GET and POST for agenda and add dashboard view

--- src/app/contacts/contact-store.ts
+++ src/app/contacts/contact-store.ts
@@ -1,10 +1,60 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Angenda } from './contact.models';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs';
+
+const URL = 'https://playground.4geeks.com/contact';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactStore {
+  private http = inject(HttpClient);
+  agenda = signal<Angenda | null>(null);
 
-  constructor() { }
+  resetAgenda() {
+    console.log('limpiando');
+    
+    this.agenda.set({ slug: '', contacts: [] });
+    console.log(this.agenda());
+    
+  }
+
+  createAgenda(name: string) {
+    return this.http.post(`${URL}/agendas/${name}`, null);
+  }
+
+  getAgenda(name: string) {
+    return this.http
+      .get<Angenda>(`${URL}/agendas/${name}`)
+      .pipe(tap((items) => this.agenda.set(items)),
+      tap((items) => console.log(items))
+      );
+  }
+
+  createContact() {
+    this.http.post(`${URL}/agendas/${name}/contacts`, null)
+  }
+
+
+  updateContact() {}
+
+  deleteContact(id: number) {
+    const slug = this.agenda()?.slug
+    this.http.delete(`${URL}/agendas/${slug}/contacts/${id}`).subscribe({
+      next : () => {
+        this.agenda.update(current => {
+          if(!current) return null;
+          return {
+            ...current, 
+            contacts: current.contacts.filter(c => c.id != id)
+          }
+        })
+      },
+      error : (error) => console.error('Error al borrar', error )
+    })
+    
+  }
+
 
 }
--- src/app/contacts/pages/agenda-setup/agenda-setup.ts
+++ src/app/contacts/pages/agenda-setup/agenda-setup.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ContactStore } from '../../contact-store';
+import { Router } from '@angular/router';
+import { catchError, tap } from 'rxjs';
 
 @Component({
   selector: 'app-agenda-setup',
   imports: [],
   templateUrl: './agenda-setup.html',
 })
-export class AgendaSetup { }
+export class AgendaSetup {
+  contactStore = inject(ContactStore);
+  private router = inject(Router);
+
+  agendaData = signal<string>('');
+  isFormReady = computed<boolean>(() => this.agendaData().trim().length <= 0);
+
+  updateName(nameInp: string) {
+    console.log(nameInp);
+    this.agendaData.set(nameInp);
+  }
+
+  onStart() {
+    const name = this.agendaData();
+
+    this.contactStore.getAgenda(name).pipe(
+        catchError((error) => {
+
+          if (error.status === 404) {
+            console.warn('Agenda no encontrada, creando...');
+            return this.contactStore.createAgenda(name)
+            .pipe(
+              tap(() => this.contactStore.resetAgenda()),
+            );
+          }
+          throw error;
+        }),
+      )
+      .subscribe({
+        next: () => {
+          console.log('Todo listo, navegando...');
+          this.router.navigate(['my-agenda/contacts']);
+        },
+        error: (err) => console.error('Error definitivo:', err),
+      });
+  }
+}
